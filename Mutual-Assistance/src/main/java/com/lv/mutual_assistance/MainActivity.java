96f75202f329754d6180902eceb145052a170892
TuoNiao666 <1985270236@qq.com>
2016-12-09 19:31:02 +0800

    ma

---
+++ Mutual-Assistance/src/main/java/com/lv/mutual_assistance/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lv.mutual_assistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        Toast.makeText(this,"进入了MA的世界！",0).show();
+    }
+}
